a29cc314331f44fc1bc222d4b3a87910c28db963
ErnestSzypula <ernest.szypula@gmail.com>
2019-12-08 20:05:58 +0100

    add steering drom file

--- main-tests/MainTests.py
+++ main-tests/MainTests.py
@@ -1,5 +1,6 @@
 import time
 import threading
+import pickle
 import os
 from imu import IMUClass
 from  MotorControl import *
@@ -14,6 +15,11 @@ from connectionOdroid import *
 # logging.basicConfig(filename='output.log', level=logging.INFO)
 
 PAD_STEERING_FLAG = True
+
+SAVE_FLAG = False
+READ_FLAG = False
+MOVES_FILE = "moves.dat"
+
 
 IP_ADDRESS_2 = '10.42.0.158'  # address jetson
 IP_ADDRESS_1 = '192.168.137.208'  # address odroid
@@ -317,6 +323,7 @@ class PadSteeringThread(threading.Thread):
         threading.Thread.__init__(self)
         self.lock = threading.Lock()
         self.pid_thread = pid_thread
+        self.last_time = time.time()
 
         # stworzenie wątków do odczytu danych z pada
         self.connection = Connection(IP_ADDRESS_2, PAD_PORT)
@@ -349,8 +356,68 @@ class PadSteeringThread(threading.Thread):
                     self.pid_thread.pid_motors_speeds_update[4] += vertical_duty/2
                     self.pid_thread.roll_PID.setSetPoint(roll_offset)
                     self.pid_thread.pitch_PID.setSetPoint(pitch_offset)
+                    if SAVE_FLAG:
+                        self.save(data_frame)
                     # print(data_frame)
                     #self.pid_thread.depth_PID.setSetPoint(depth_offset)  # can't use without depth funcionalities
+
+    def save(self, data_frame):
+        with open(MOVES_FILE, 'ab') as file:
+            data_frame.append(time.time()-self.last_time)
+            self.last_time = time.time()
+            file.write(pickle.dumps(data_frame))
+
+
+class ReadSteeringThread(threading.Thread):
+    """Thread class that play primitive steering by commands from file """
+    def __init__(self, pid_thread):
+        threading.Thread.__init__(self)
+        self.lock = threading.Lock()
+        self.pid_thread = pid_thread
+        self.file = None
+
+        # strojenie regulatorow #hardcoded
+        pid_thread.roll_PID.setPIDCoefficients(4, 2, 2) # zahardkodowane narazie # nastawy  z testow
+        pid_thread.pitch_PID.setPIDCoefficients(10, 2, 1)
+        pid_thread.yaw_PID.setPIDCoefficients(0, 0, 0) # zera, bo horyzontalnymi silnikami sterujemy tylko plikiem
+
+    def run(self):
+        global motors_speed, run_flag
+        self.file = open(MOVES_FILE, "rb")
+        while True:
+            with self.lock:
+                try:
+                    data_frame = pickle.load(self.file)
+                    #print(data_frame)
+                except(EOFError, pickle.UnpicklingError):
+                    self.pid_thread.pid_motors_speeds_update[0] = 0
+                    self.pid_thread.pid_motors_speeds_update[1] = 0
+                    self.pid_thread.pid_motors_speeds_update[2] = 0
+                    self.pid_thread.pid_motors_speeds_update[3] = 0
+                    self.pid_thread.pid_motors_speeds_update[4] = 0
+                    #print("KONIEC")
+                    break
+
+                if len(data_frame) == 6:
+                    motor_0_duty = data_frame[0]
+                    motor_1_duty = data_frame[1]
+                    roll_offset = data_frame[2]
+                    pitch_offset = data_frame[3]
+                    #depth_offset = data_frame[4]
+                    vertical_duty = data_frame[4] # bez glebokosciomierza
+
+                    time.sleep(data_frame[5]) # usypiamy na dany czas
+
+                    self.pid_thread.pid_motors_speeds_update[0] += motor_0_duty
+                    self.pid_thread.pid_motors_speeds_update[1] += motor_1_duty
+                    self.pid_thread.pid_motors_speeds_update[2] += vertical_duty
+                    self.pid_thread.pid_motors_speeds_update[3] += vertical_duty/2
+                    self.pid_thread.pid_motors_speeds_update[4] += vertical_duty/2
+                    self.pid_thread.roll_PID.setSetPoint(roll_offset)
+                    self.pid_thread.pitch_PID.setSetPoint(pitch_offset)
+                    # print(data_frame)
+                    #self.pid_thread.depth_PID.setSetPoint(depth_offset)  # can't use without depth funcionalities
+
 
 
 
@@ -370,6 +437,9 @@ pid_thread.setIMU(imu)
 ui_thread = UIThread(pid_thread)
 if PAD_STEERING_FLAG:
     pad_steering_thread = PadSteeringThread(pid_thread)
+if READ_FLAG:
+    read_steering_flag = ReadSteeringThread(pid_thread)
+
 
 # opening another terminal and executing output.log tailing
 # os.system("gnome-terminal -e 'tail -f output.log'")   # works on PC Ubuntu
@@ -383,6 +453,7 @@ motors_control_thread.start()
 pid_thread.start()
 if PAD_STEERING_FLAG:
     pad_steering_thread.start()
-
+if READ_FLAG:
+    read_steering_flag.start()
 
 ui_thread.start()
